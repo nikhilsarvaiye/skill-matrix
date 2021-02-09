e188250c621da04288ad61d41f993ab865916b5e
Nikhil Sarvaiye <nikhils@nitorinfotech.com>
2021-02-10 00:28:42 +0530

    Modularized ODataQueryBuilder

--- backend/Api/Controllers/SkillController.cs
+++ backend/Api/Controllers/SkillController.cs
@@ -3,6 +3,8 @@
     using System;
     using System.Collections.Generic;
     using System.Threading.Tasks;
+    using Common.Helpers;
+    using Common.Models;
     using Microsoft.AspNet.OData.Builder;
     using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Mvc;
@@ -25,84 +27,22 @@
         [HttpGet]
         public async Task<IEnumerable<Skill>> GetAsync()
         {
-            var parser = new ODataUriParser(this.GetEdmModel(), this.GetServiceRoot(this.Request), this.GetRelativeUri(this.Request));
-            var filterClause = parser.ParseFilter();
-            var selectExpandClause = parser.ParseSelectAndExpand();
-            // var b = new ODataQueryOptionParser(this.GetEdmModel(), parser.ParsePath(), parser.CustomQueryOptions);
+            return await this._skillService.GetAsync();
+        }
 
-            // Parsing a filter, e.g. /Products?$filter=Name eq 'beer' 
-            if (filterClause != null)
-            {
-                var binaryOperator = filterClause.Expression as BinaryOperatorNode;
-                if (binaryOperator != null)
-                {
-                    var property = binaryOperator.Left as SingleValuePropertyAccessNode ?? binaryOperator.Right as SingleValuePropertyAccessNode;
-                    var constant = binaryOperator.Left as ConstantNode ?? binaryOperator.Right as ConstantNode;
+        [HttpGet("paginate")]
+        public async Task<PaginationResponse<Skill>> PaginateAsync(PaginationCriteria<Skill> paginationCriteria)
+        {
+            // todo: need to get standard odata working properly
+            // paginationCriteria = this.Request.ToPaginationCriteria<Skill>();
 
-                    if (property != null && property.Property != null && constant != null && constant.Value != null)
-                    {
-                        Console.WriteLine("Property: " + property.Property.Name);
-                        Console.WriteLine("Operator: " + binaryOperator.OperatorKind);
-                        Console.WriteLine("Value: " + constant.LiteralText);
-                    }
-                }
-
-                var inOperator = filterClause.Expression as InNode;
-                if (inOperator != null)
-                {
-                    var property = inOperator.Left as SingleValueNode;
-                    var constant = inOperator.Right as CollectionNode;
-
-                    Console.WriteLine("Property: " + inOperator.Left.ToString());
-                    Console.WriteLine("Operator: " + inOperator.Kind);
-                    Console.WriteLine("Value: " + inOperator.Right.ToString());
-                }
-            }
-
-            // Parsing a select list , e.g. /Products?$select=Price,Name
-            if (selectExpandClause != null)
-            {
-                foreach (var item in selectExpandClause.SelectedItems)
-                {
-                    var selectItem = item as PathSelectItem;
-                    if (selectItem != null && selectItem.SelectedPath != null)
-                    {
-                        var segment = selectItem.SelectedPath.FirstSegment as PropertySegment;
-                        if (segment != null)
-                        {
-                            Console.WriteLine("Property: " + segment.Property.Name);
-                        }
-                    }
-                }
-            }
-            
-            var result = await this._skillService.GetAsync();
-
-            return result;
+            return await this._skillService.PaginateAsync(paginationCriteria);
         }
 
         [HttpPost]
         public async Task<Skill> CreateAsync(Skill skill)
         {
             return await this._skillService.CreateAsync(skill);
-        }
-
-        private Uri GetServiceRoot(HttpRequest httpRequest)
-        {
-            return new Uri("https://services.odata.org/V4/OData/OData.svc");
-        }
-
-        private Uri GetRelativeUri(HttpRequest httpRequest)
-        {
-            return new Uri(string.Format("https://services.odata.org/V4/OData/OData.svc/Skills?$select=Name,Id&$filter=Name in ('Milk', 'Cheese', 'Donut')", httpRequest.QueryString));
-        }
-
-        private IEdmModel GetEdmModel()
-        {
-            var odataBuilder = new ODataConventionModelBuilder();
-            odataBuilder.EntitySet<Skill>("Skills");
-
-            return odataBuilder.GetEdmModel();
         }
     }
 }
---
+++ backend/Common/Helpers/ODataQueryBuilder.cs
@@ -0,0 +1,88 @@
+﻿namespace Common.Helpers
+{
+    using Microsoft.AspNet.OData.Builder;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.OData.Edm;
+    using Microsoft.OData.UriParser;
+    using Models;
+    using System;
+    
+    public static class ODataQueryBuilder
+    {
+        public static PaginationCriteria<T> ToPaginationCriteria<T>(this HttpRequest httpRequest)
+            where T: class, new()
+        {
+            var parser = new ODataUriParser(GetEdmModel<T>(), GetServiceRoot(httpRequest), GetRelativeUri(httpRequest));
+            var filterClause = parser.ParseFilter();
+            var selectExpandClause = parser.ParseSelectAndExpand();
+            // var b = new ODataQueryOptionParser(this.GetEdmModel(), parser.ParsePath(), parser.CustomQueryOptions);
+
+            // Parsing a filter, e.g. /Products?$filter=Name eq 'beer' 
+            if (filterClause != null)
+            {
+                var binaryOperator = filterClause.Expression as BinaryOperatorNode;
+                if (binaryOperator != null)
+                {
+                    var property = binaryOperator.Left as SingleValuePropertyAccessNode ?? binaryOperator.Right as SingleValuePropertyAccessNode;
+                    var constant = binaryOperator.Left as ConstantNode ?? binaryOperator.Right as ConstantNode;
+
+                    if (property != null && property.Property != null && constant != null && constant.Value != null)
+                    {
+                        Console.WriteLine("Property: " + property.Property.Name);
+                        Console.WriteLine("Operator: " + binaryOperator.OperatorKind);
+                        Console.WriteLine("Value: " + constant.LiteralText);
+                    }
+                }
+
+                var inOperator = filterClause.Expression as InNode;
+                if (inOperator != null)
+                {
+                    var property = inOperator.Left as SingleValueNode;
+                    var constant = inOperator.Right as CollectionNode;
+
+                    Console.WriteLine("Property: " + inOperator.Left.ToString());
+                    Console.WriteLine("Operator: " + inOperator.Kind);
+                    Console.WriteLine("Value: " + inOperator.Right.ToString());
+                }
+            }
+
+            // Parsing a select list , e.g. /Products?$select=Price,Name
+            if (selectExpandClause != null)
+            {
+                foreach (var item in selectExpandClause.SelectedItems)
+                {
+                    var selectItem = item as PathSelectItem;
+                    if (selectItem != null && selectItem.SelectedPath != null)
+                    {
+                        var segment = selectItem.SelectedPath.FirstSegment as PropertySegment;
+                        if (segment != null)
+                        {
+                            Console.WriteLine("Property: " + segment.Property.Name);
+                        }
+                    }
+                }
+            }
+
+            return new PaginationCriteria<T>();
+        }
+
+        private static IEdmModel GetEdmModel<T>()
+            where T : class, new()
+        {
+            var odataBuilder = new ODataConventionModelBuilder();
+            odataBuilder.EntitySet<T>("Skills");
+
+            return odataBuilder.GetEdmModel();
+        }
+
+        private static Uri GetServiceRoot(HttpRequest httpRequest)
+        {
+            return new Uri("https://services.odata.org/V4/OData/OData.svc");
+        }
+
+        private static Uri GetRelativeUri(HttpRequest httpRequest)
+        {
+            return new Uri(string.Format("https://services.odata.org/V4/OData/OData.svc/Skills?$select=Name,Id&$filter=Name in ('Milk', 'Cheese', 'Donut')", httpRequest.QueryString));
+        }
+    }
+}
